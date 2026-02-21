e5c90270d5494966beb7e13cfcafe33eceb56d41
Lenovo <Lenovo@DESKTOP-63GO087>
2026-02-21 14:25:37 +0100

    feat: add mobilenetv2 transfer model

---
+++ src/fruitcls/models/mobilenet_transfer.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+def build_mobilenet_transfer(input_shape=(224, 224, 3), num_classes=6, trainable_base=False) -> tf.keras.Model:
+    base = tf.keras.applications.MobileNetV2(
+        include_top=False,
+        weights="imagenet",
+        input_shape=input_shape,
+    )
+    base.trainable = trainable_base
+
+    inputs = tf.keras.Input(shape=input_shape)
+    x = tf.keras.applications.mobilenet_v2.preprocess_input(inputs)
+    x = base(x, training=False)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    outputs = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
+
+    return tf.keras.Model(inputs, outputs, name="mobilenetv2_transfer")
